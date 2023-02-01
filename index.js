f7bbee80c7a00f437e1107c2d2ba3274a619b97d
Jorge Meneses <jorgeluis.menesespaternina@gmail.com>
2023-01-31 20:05:31 -0500

    actCSS

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const hamburguer = document.querySelector("#hamburguesa");
 const close = document.querySelector("#close");
 const menu = document.querySelector("#menu");
-
+const info = document.querySelector("#info");
+const cards = document.querySelector("#cards");
+const articles = document.querySelector("#articles");
+const news = document.querySelector("#news");
 
 hamburguer.addEventListener('click', () => {
 
     close.style.display = 'block';
     hamburguer.style.display = 'none';
-
+    // ]info.style.opacity = '0.5';
     menu.style.display = 'block';
+    info.classList.add('opacity');
+    articles.classList.add('opacity');
+    cards.classList.add('opacity');
+    news.classList.add('opacity');
     menu.style.transtion = 'all 0.5s ease'
+
 
 });
 
@@ -17,7 +25,10 @@ close.addEventListener('click', () => {
 
     close.style.display = 'none';
     hamburguer.style.display = 'block';
-
+    info.classList.remove('opacity');
+    cards.classList.remove('opacity');
+    articles.classList.remove('opacity');
+    news.classList.remove('opacity');
     menu.style.display = 'none';
 
 
